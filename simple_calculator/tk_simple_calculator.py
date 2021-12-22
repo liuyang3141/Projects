776443ceaf2b5ae4f45f28ee39fc778c67038852
Yang Liu <liu.yang3.141@gmail.com>
2021-12-22 11:58:50 -0800

    Numbers longer than 1 digit is now working. Decimals are also working.

--- simple_calculator/tk_simple_calculator.py
+++ simple_calculator/tk_simple_calculator.py
@@ -24,62 +24,159 @@ def calculate(calc):
         calc.remove('(')
         calc.remove(')')
 
-    operatorIndex = 0
+    operatorIndex1 = 0
+    operatorIndex2 = 0
 
-    while 'sqr' in calc:
-        operatorIndex = calc.index('sqr')
-        calc[operatorIndex - 1] = str(float(calc[operatorIndex -1]) * float(calc[operatorIndex -1]))
-        del calc[operatorIndex]
+    if 'sqr' in calc and 'sqrt' in calc:
+        while 'sqr' in calc and 'sqrt' in calc:
+            operatorIndex1 = calc.index('sqr')
+            operatorIndex2 = calc.index('sqrt')
 
-    while 'sqrt' in calc:
-        operatorIndex = calc.index('sqrt')
-        calc[operatorIndex - 1] = str(math.sqrt(float(calc[operatorIndex -1])))
-        del calc[operatorIndex]
+            if operatorIndex1 < operatorIndex2:
+                calc[operatorIndex1 - 1] = str(float(calc[operatorIndex1 -1]) * float(calc[operatorIndex1 -1]))
+                del calc[operatorIndex1]
+            elif operatorIndex2 < operatorIndex1:
+                calc[operatorIndex2 - 1] = str(math.sqrt(float(calc[operatorIndex2 -1])))
+                del calc[operatorIndex2]
 
-    while '*' in calc:
-        operatorIndex = calc.index('*')
-        calc[operatorIndex - 1] = str(float(calc[operatorIndex - 1]) * float(calc[operatorIndex + 1]))
-        del calc[operatorIndex]
-        del calc[operatorIndex]
+    if 'sqr' in calc:
+        while 'sqr' in calc:
+            operatorIndex1 = calc.index('sqr')
+            calc[operatorIndex1 - 1] = str(float(calc[operatorIndex1 -1]) * float(calc[operatorIndex1 -1]))
+            del calc[operatorIndex1]
+    if 'sqrt' in calc:
+        while 'sqrt' in calc:
+            operatorIndex2 = calc.index('sqrt')
+            calc[operatorIndex2 - 1] = str(math.sqrt(float(calc[operatorIndex2 -1])))
+            del calc[operatorIndex2]
 
-    while '/' in calc:
-        operatorIndex = calc.index('/')
-        calc[operatorIndex - 1] = str(float(calc[operatorIndex - 1]) / float(calc[operatorIndex + 1]))
-        del calc[operatorIndex]
-        del calc[operatorIndex]
 
-    while '+' in calc:
-        operatorIndex = calc.index('+')
-        calc[operatorIndex - 1] = str(float(calc[operatorIndex - 1]) + float(calc[operatorIndex + 1]))
-        del calc[operatorIndex]
-        del calc[operatorIndex]
+    if '*' in calc and '/' in calc:
+        while '*' in calc and '/' in calc:
+            operatorIndex1 = calc.index('*')
+            operatorIndex2 = calc.index('/')
 
-    while '-' in calc:
-        operatorIndex = calc.index('-')
-        calc[operatorIndex - 1] = str(float(calc[operatorIndex - 1]) - float(calc[operatorIndex + 1]))
-        del calc[operatorIndex]
-        del calc[operatorIndex]
+            if operatorIndex1 < operatorIndex2:
+                calc[operatorIndex1 - 1] = str(float(calc[operatorIndex1 - 1]) * float(calc[operatorIndex1 + 1]))
+                del calc[operatorIndex1]
+                del calc[operatorIndex1]
+            elif operatorIndex2 < operatorIndex1:
+                calc[operatorIndex2 - 1] = str(float(calc[operatorIndex2 - 1]) / float(calc[operatorIndex2 + 1]))
+                del calc[operatorIndex2]
+                del calc[operatorIndex2]
 
+    if '*' in calc:
+        while '*' in calc:
+            operatorIndex1 = calc.index('*')
+            calc[operatorIndex1 - 1] = str(float(calc[operatorIndex1 - 1]) * float(calc[operatorIndex1 + 1]))
+            del calc[operatorIndex1]
+            del calc[operatorIndex1]
+    if '/' in calc:
+        while '/' in calc:
+            operatorIndex2 = calc.index('/')
+            calc[operatorIndex2 - 1] = str(float(calc[operatorIndex2 - 1]) / float(calc[operatorIndex2 + 1]))
+            del calc[operatorIndex2]
+            del calc[operatorIndex2]
+
+    if '+' in calc and '-' in calc:
+        while '+' in calc and '-' in calc:
+            operatorIndex1 = calc.index('+')
+            operatorIndex2 = calc.index('-')
+
+            if operatorIndex1 < operatorIndex2:
+                calc[operatorIndex1 - 1] = str(float(calc[operatorIndex1 - 1]) + float(calc[operatorIndex1 + 1]))
+                del calc[operatorIndex1]
+                del calc[operatorIndex1]
+            elif operatorIndex2 < operatorIndex1:
+                calc[operatorIndex2 - 1] = str(float(calc[operatorIndex2 - 1]) - float(calc[operatorIndex2 + 1]))
+                del calc[operatorIndex2]
+                del calc[operatorIndex2]
+
+    if '+' in calc:
+        while '+' in calc:
+            operatorIndex1 = calc.index('+')
+            calc[operatorIndex1 - 1] = str(float(calc[operatorIndex1 - 1]) + float(calc[operatorIndex1 + 1]))
+            del calc[operatorIndex1]
+            del calc[operatorIndex1]
+    if '-' in calc:
+        while '-' in calc:
+            operatorIndex2 = calc.index('-')
+            calc[operatorIndex2 - 1] = str(float(calc[operatorIndex2 - 1]) - float(calc[operatorIndex2 + 1]))
+            del calc[operatorIndex2]
+            del calc[operatorIndex2]
     return str(calc[0])
 
-f_num = 0.0
 operator = ""
 calc = []
 numUndos = 100
 undo = []
+line = ""
 
 def button_click(number):
+    global calc
+    global undo
+    global line
+    
+    if number.isdigit() or number == '.':
+        line += str(number)
+    else:
+        if line != "":
+            calc.append(line)
+        calc.append(number)
+
+        if len(undo) >= numUndos - 2:
+            undo = undo[2:len(undo)]
+
+        if line != "":
+            undo.append(line)
+        undo.append(number)
+        line = ""
+    for index in range(len(calc)):
+        print(calc[index])
     current = entry.get()
     entry.delete(0, tk.END)
-    calc.append(number)
-    entry.insert(0, str(current) + str(number))
-    global undo
+    entry.insert(0, current + number)
 
-    if len(undo) <= numUndos:
-        undo.append(str(number).lstrip('\n'))
+def button_equal():
+    global calc
+    global undo
+    global line
+    current = entry.get()
+    if line != "":
+        calc.append(line)
+    line = ""
+    parse(calc)
+    display.insert(tk.END, "\n" + current + " = " + calc[0])
+    display.see("end")
+    entry.delete(0, tk.END)
+    entry.insert(0, calc[0])
+    undo.append('=')
+    undo.append(calc[0])
+    
+def button_undo():
+    global undo
+    global calc
+   
+    entry.delete(0, tk.END)
+
+    if undo[len(undo) - 2] == '=':
+        undo = undo[:len(undo) - 2]
     else:
-        del undo[0]
-        undo.append(str(number).lstrip('\n'))
+        del undo[len(undo) - 1]
+
+    if '=' in undo:
+        startIndex = findLastOccurence(undo, '=') + 1
+    else:
+        startIndex = 0
+
+    calc.clear()
+    line = ""
+
+    for index in range(startIndex, len(undo)):
+        line += str(undo[index])
+        calc.append(undo[index])
+
+    entry.insert(0, line)
 
 def button_add():
     global operator
@@ -116,43 +213,6 @@ def button_sqrt():
     operator = 'sqrt'
     button_click(operator)
 
-def button_undo():
-    global undo
-    global calc
-   
-    entry.delete(0, tk.END)
-
-    if undo[len(undo) - 2] == '=':
-        undo = undo[:len(undo) - 2]
-    else:
-        del undo[len(undo) - 1]
-
-    if '=' in undo:
-        startIndex = findLastOccurence(undo, '=') + 1
-    else:
-        startIndex = 0
-
-    calc.clear()
-    line = ""
-
-    for index in range(startIndex, len(undo)):
-        line += str(undo[index])
-        calc.append(undo[index])
-
-    entry.insert(0, line)
-
-def button_equal():
-    global calc
-    global undo
-    current = entry.get()
-    parse(calc)
-    display.insert(tk.END, "\n" + current + " = " + calc[0])
-    display.see("end")
-    entry.delete(0, tk.END)
-    entry.insert(0, calc[0])
-    undo.append('=')
-    undo.append(calc[0].lstrip('\n'))
-
 def button_clear():
     entry.delete(0, tk.END)
     calc.clear()
@@ -167,25 +227,25 @@ def returnPressed(event):
 window = tk.Tk()
 window.title("Simple Calculator")
 
-display = tk.Text(master = window, height = 5, width = 35, borderwidth = 1)
+display = tk.Text(master = window, height = 5, width = 35, borderwidth = 0)
 display.grid(row = 0, column = 0, rowspan = 2, columnspan = 6, sticky = 'new')
 
-entry = tk.Entry(master = window, width = 35, borderwidth = 5)
-entry.grid(row = 3, column = 0, columnspan = 6, padx = 10, pady = 10, sticky = 'new')
+entry = tk.Entry(master = window, width = 35, borderwidth = 0, font = ('Arial 20'))
+entry.grid(row = 3, column = 0, columnspan = 6, sticky = 'new')
 entry.grid_rowconfigure(1, weight = 1)
 entry.grid_columnconfigure(0, weight = 1)
 
 # Define buttons
-btn_1 = tk.Button(master = window, text= "1", padx = 20, pady = 10, command = lambda: button_click(1))
-btn_2 = tk.Button(master = window, text= "2", padx = 20, pady = 10, command = lambda: button_click(2)) 
-btn_3 = tk.Button(master = window, text= "3", padx = 20, pady = 10, command = lambda: button_click(3)) 
-btn_4 = tk.Button(master = window, text= "4", padx = 20, pady = 10, command = lambda: button_click(4)) 
-btn_5 = tk.Button(master = window, text= "5", padx = 20, pady = 10, command = lambda: button_click(5)) 
-btn_6 = tk.Button(master = window, text= "6", padx = 20, pady = 10, command = lambda: button_click(6)) 
-btn_7 = tk.Button(master = window, text= "7", padx = 20, pady = 10, command = lambda: button_click(7)) 
-btn_8 = tk.Button(master = window, text= "8", padx = 20, pady = 10, command = lambda: button_click(8)) 
-btn_9 = tk.Button(master = window, text= "9", padx = 20, pady = 10, command = lambda: button_click(9)) 
-btn_0 = tk.Button(master = window, text= "0", padx = 20, pady = 10, command = lambda: button_click(0))
+btn_1 = tk.Button(master = window, text= "1", padx = 20, pady = 10, command = lambda: button_click('1'))
+btn_2 = tk.Button(master = window, text= "2", padx = 20, pady = 10, command = lambda: button_click('2')) 
+btn_3 = tk.Button(master = window, text= "3", padx = 20, pady = 10, command = lambda: button_click('3')) 
+btn_4 = tk.Button(master = window, text= "4", padx = 20, pady = 10, command = lambda: button_click('4')) 
+btn_5 = tk.Button(master = window, text= "5", padx = 20, pady = 10, command = lambda: button_click('5')) 
+btn_6 = tk.Button(master = window, text= "6", padx = 20, pady = 10, command = lambda: button_click('6')) 
+btn_7 = tk.Button(master = window, text= "7", padx = 20, pady = 10, command = lambda: button_click('7')) 
+btn_8 = tk.Button(master = window, text= "8", padx = 20, pady = 10, command = lambda: button_click('8')) 
+btn_9 = tk.Button(master = window, text= "9", padx = 20, pady = 10, command = lambda: button_click('9')) 
+btn_0 = tk.Button(master = window, text= "0", padx = 20, pady = 10, command = lambda: button_click('0'))
 btn_dot = tk.Button(master = window, text= ".", padx = 20, pady = 10, command = lambda: button_click('.'))
 btn_open_par = tk.Button(master = window, text= "(", padx = 20, pady = 10, command = lambda: button_click('('))
 btn_close_par = tk.Button(master = window, text= ")", padx = 20, pady = 10, command = lambda: button_click(')'))
